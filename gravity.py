a075ead4e76e5118b289d8f9dcbf9d591c572123
Cesar1357 <85760661+Cesar1357@users.noreply.github.com>
2022-10-28 19:48:40 -0500

    Add files via upload

---
+++ gravity.py
@@ -0,0 +1,31 @@
+from ast import Lambda
+import csv
+from dataclasses import replace
+import pandas as pd
+
+info = pd.read_csv('./final_df.csv')
+planets = pd.DataFrame(info)
+
+planets['Radius']=planets['Radius'].to_string()
+planets['Radius']=planets['Radius'].apply(lambda  x: x.replace('$', '').replace(',', '')).astype('float')
+
+radius = planets['Radius'].to_list()
+mass = planets['Mass'].to_list()
+gravity = []
+
+def convert_to_si(radius,mass):
+  for i in range(0,len(radius)-1):
+    radius[i] = radius[i]*6.957e+8
+    mass[i] = mass[i]*1.989e+30
+
+convert_to_si(radius,mass)
+
+def gravity_calculation(radius,mass):
+  G = 6.674e-11
+  for index in range(0,len(mass)):
+    g = (mass[index]*G)/((radius[index])**2)
+    gravity.append(g)
+
+gravity_calculation(radius,mass)
+
+planets['Gravity'] = gravity
